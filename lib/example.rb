518fad8354412c385189ff2fbe3c87b01b80b6a6
+ <+>
2023-02-12 18:25:30 +0300

    done exmaple

---
+++ lib/example.rb
@@ -0,0 +1,16 @@
+#  If we have a Person class, our setter and getter for the name 
+#  attribute will look like this:
+
+class Person
+
+  # setter method
+  def name=(name)
+    @name = name
+  end
+
+  # getter method
+  def name
+    @name
+  end
+
+end
